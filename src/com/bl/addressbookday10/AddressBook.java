5fe96667a92bb9aeb2543e64b93687aa74b64ff3
lunainfotec <kirtimadan1997@gmail.com>
2023-05-23 20:06:48 +0530

    added:UC2

---
+++ src/com/bl/addressbookday10/AddressBook.java
@@ -0,0 +1,45 @@
+package com.bl.addressbookday10;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook{
+
+
+    Scanner sc = new Scanner(System.in);
+    ArrayList<Contacts> contactArrayList = new ArrayList<Contacts>();
+    public void addContact() {
+
+        Contacts contact = new Contacts();
+
+        System.out.println("Enter the First Name : ");
+        contact.setFirstName(sc.next());
+        System.out.println("Enter the Last Name : ");
+        contact.setLastName(sc.next());
+        System.out.println("Enter the Address: ");
+        contact.setAddress(sc.next());
+        System.out.println("Enter the City : ");
+        contact.setCity(sc.next());
+        System.out.println("Enter the State : ");
+        contact.setState(sc.next());
+        System.out.println("Enter the Zip code : ");
+        contact.setZipcode(sc.nextDouble());
+        System.out.println("Enter the Mobile Number : ");
+        contact.setPhoneNumber(sc.nextDouble());
+        System.out.println("Enter the Email ID : ");
+        contact.setEmail(sc.next());
+
+        contactArrayList.add(contact);
+    }
+
+
+    public void displayContact() {
+        for (int i = 0; i < contactArrayList.size(); i++) {
+            Contacts contact = contactArrayList.get(i);
+            System.out.println(contact.toString());
+        }
+
+    }
+
+
+}
--- src/com/bl/addressbookday10/AddressBookMain.java
+++ src/com/bl/addressbookday10/AddressBookMain.java
@@ -1,7 +1,37 @@
 package com.bl.addressbookday10;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        AddressBook addressBook = new AddressBook();
+        Scanner sc = new Scanner(System.in);
+
+        int Number;
+
+        do {
+            System.out.println(" 1. Add Contact \n 2. Display contact ");
+            System.out.println("Enter the  Number: ");
+
+            Number = sc.nextInt();
+
+            switch (Number) {
+
+                case 1:
+                    addressBook.addContact();
+                    break;
+                case 2:
+                    addressBook.displayContact();
+                    break;
+                default:
+                    System.out.println(" Invalid Input!!! ");
+                    break;
+
+            }
+        } while (Number != 2);
     }
+
 }
+
