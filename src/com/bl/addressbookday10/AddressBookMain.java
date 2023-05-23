1d9364a982bbfeb62ac2aff023b047dc847c6c32
lunainfotec <kirtimadan1997@gmail.com>
2023-05-23 19:40:37 +0530

    added: welcome message

---
+++ src/com/bl/addressbookday10/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bl.addressbookday10;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
